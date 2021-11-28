649ec861aeecaa44cb6bfc4a4c441b471754382c
hpgbproductions <36079650+hpgbproductions@users.noreply.github.com>
2021-11-28 19:13:50 +0800

    Add files via upload

---
+++ Assets/FloodedRailScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloodedRailScript : MonoBehaviour
+{
+    [SerializeField] private Transform RailParentTransform;
+    [SerializeField] private Transform RailTransform;
+    [SerializeField] private Transform CenterColliderTransform;
+    [SerializeField] private Transform AircraftTrackerTransform;
+
+    [SerializeField] private Transform PlatformTransform;
+
+    [SerializeField] private Transform BrightTransform;
+    [SerializeField] private Renderer BrightRenderer;
+    [SerializeField] private Renderer BrightRendererSurrounding;
+
+    [SerializeField] private RailCubeMaker RailCubeMaker;
+
+    bool WaterActive = true;
+
+    private void Awake()
+    {
+        ServiceProvider.Instance.DevConsole.RegisterCommand<float>("SetRailGauge", SetRailScale);
+        ServiceProvider.Instance.DevConsole.RegisterCommand<float>("SetRailHeading", SetRailHeading);
+        ServiceProvider.Instance.DevConsole.RegisterCommand<float>("SetBrightPlaneDepth", SetBrightPlaneDepth);
+        ServiceProvider.Instance.DevConsole.RegisterCommand<float, float, float>("SetBrightPlaneColor", SetBrightPlaneColor);
+        ServiceProvider.Instance.DevConsole.RegisterCommand<float, float, float>("TranslatePlatform", TranslatePlatform);
+        ServiceProvider.Instance.DevConsole.RegisterCommand("ToggleWater", ToggleWater);
+        ServiceProvider.Instance.DevConsole.RegisterCommand<float, float>("GenerateRailCubes", GenerateRailCubes);
+    }
+
+    private void SetRailScale(float scale)
+    {
+        RailTransform.localScale = new Vector3(scale, 1f, scale);
+    }
+
+    private void SetRailHeading(float heading)
+    {
+        AircraftTrackerTransform.position = ServiceProvider.Instance.PlayerAircraft.MainCockpitPosition;
+        AircraftTrackerTransform.eulerAngles = ServiceProvider.Instance.PlayerAircraft.MainCockpitRotation;
+        RailParentTransform.localEulerAngles = new Vector3(0f, heading, 0f);
+        CenterColliderTransform.localEulerAngles = new Vector3(0f, heading, 0f);
+        ServiceProvider.Instance.PlayerAircraft.MainCockpitRotation = AircraftTrackerTransform.eulerAngles;
+        ServiceProvider.Instance.PlayerAircraft.MainCockpitPosition = AircraftTrackerTransform.position;
+    }
+
+    private void SetBrightPlaneDepth(float depth)
+    {
+        BrightTransform.localPosition = new Vector3(0f, -depth, 0f);
+    }
+
+    private void SetBrightPlaneColor(float r, float g, float b)
+    {
+        Color c = new Color(r, g, b, 1f);
+        BrightRenderer.material.color = c;
+        BrightRendererSurrounding.material.color = c;
+    }
+
+    private void ToggleWater()
+    {
+        if (WaterActive)
+            ServiceProvider.Instance.GameWorld.SeaLevel = null;
+        else
+            ServiceProvider.Instance.GameWorld.SeaLevel = 0f;
+
+        WaterActive = !WaterActive;
+    }
+
+    private void TranslatePlatform(float x, float y, float z)
+    {
+        PlatformTransform.Translate(new Vector3(x, y, z), Space.Self);
+    }
+
+    private void GenerateRailCubes(float origin, float range)
+    {
+        Vector3 rcmPos = RailCubeMaker.transform.localPosition;
+        RailCubeMaker.transform.localPosition = new Vector3(rcmPos.x, rcmPos.y, origin);
+        RailCubeMaker.RegenerateCubes(range);
+    }
+
+    private void OnDestroy()
+    {
+        ServiceProvider.Instance.DevConsole.UnregisterCommand("SetRailGauge");
+        ServiceProvider.Instance.DevConsole.UnregisterCommand("SetRailHeading");
+        ServiceProvider.Instance.DevConsole.UnregisterCommand("SetBrightPlaneDepth");
+        ServiceProvider.Instance.DevConsole.UnregisterCommand("SetBrightPlaneColor");
+        ServiceProvider.Instance.DevConsole.UnregisterCommand("TranslatePlatform");
+        ServiceProvider.Instance.DevConsole.UnregisterCommand("ToggleWater");
+        ServiceProvider.Instance.DevConsole.UnregisterCommand("GenerateRailCubes");
+    }
+}
---
+++ Assets/RailCubeMaker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailCubeMaker : MonoBehaviour
+{
+    [SerializeField] private GameObject CubePrefab;
+
+    [SerializeField] private float spacing;
+    [SerializeField] private float range;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    public void RegenerateCubes(float range)
+    {
+        this.range = range;
+        Generate();
+    }
+
+    private void Generate()
+    {
+        // Destroy previous GameObjects
+        List<Transform> ChildTransforms = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            ChildTransforms.Add(transform.GetChild(i));
+        }
+        foreach (Transform ct in ChildTransforms)
+        {
+            Destroy(ct.gameObject);
+        }
+
+        // Start creating new GameObjects
+        Quaternion ZeroRotation = Quaternion.Euler(Vector3.zero);
+        int newCount = 1;
+        InstantiateLocal(CubePrefab, Vector3.zero, ZeroRotation);
+        for (float z = spacing; z < range; z += spacing)
+        {
+            InstantiateLocal(CubePrefab, new Vector3(0, 0, z), ZeroRotation);
+            InstantiateLocal(CubePrefab, new Vector3(0, 0, -z), ZeroRotation);
+            newCount += 2;
+        }
+        Debug.Log($"RailCubeMaker initialized {newCount} GameObjects");
+    }
+
+    private GameObject InstantiateLocal(GameObject prefab, Vector3 localPosition, Quaternion localRotation)
+    {
+        GameObject current = Instantiate(prefab, transform);
+        current.transform.localPosition = localPosition;
+        current.transform.localRotation = localRotation;
+        return current;
+    }
+}
